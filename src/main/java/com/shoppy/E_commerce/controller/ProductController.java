1a904aa64866d3d77fb41989782b2db383c8cebe
Nitish0904 <ncpothukuchi.9@gmail.com>
2025-02-13 18:55:00 -0700

    First Commit

---
+++ src/main/java/com/shoppy/E_commerce/controller/ProductController.java
@@ -0,0 +1,84 @@
+package com.shoppy.E_commerce.controller;
+
+import com.shoppy.E_commerce.model.Product;
+import com.shoppy.E_commerce.service.ProductService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+public class ProductController
+{
+    @Autowired
+    private ProductService service;
+
+    @GetMapping("/api/products")
+    public ResponseEntity<List<Product>> getAllProducts(){
+        return new ResponseEntity<>(service.getAllProducts(), HttpStatus.OK);
+    }
+
+    @GetMapping("/api/product/{id}")
+    public ResponseEntity <Product> getProduct(@PathVariable int id) {
+        Product product = service.getProductById(id);
+
+        if (product != null) {
+            return new ResponseEntity<>(product, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping("/api/product")
+    public ResponseEntity<?> addProduct(@RequestPart Product product,
+                                        @RequestPart MultipartFile imageFile){
+        try {
+            Product product1 = service.addProduct(product, imageFile);
+            return new ResponseEntity<>(product1, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        }
+    @GetMapping("/api/product/{productId}/image")
+    public ResponseEntity<byte[]> getImageByProductId(@PathVariable int productId){
+        Product product = service.getProductById(productId);
+        byte[] imageFile = product.getImageDate();
+        return ResponseEntity.ok()
+                .contentType(MediaType.valueOf(product.getImageType()))
+                .body(imageFile);
+    }
+
+    @PutMapping("/api/product/{id}")
+    public ResponseEntity<String> updateProduct(@PathVariable int id, @RequestPart Product product,
+                                                @RequestPart MultipartFile imageFile){
+        Product product1 = service.updateProduct(id, product, imageFile);
+        if (product1 != null)
+            return new ResponseEntity<>("Updated", HttpStatus.OK);
+        else return new ResponseEntity<>("Error Updating", HttpStatus.BAD_REQUEST);
+    }
+
+    @DeleteMapping("api/product/{id}")
+    public ResponseEntity<String> deleteProduct(@PathVariable int id){
+        Product product = service.getProductById(id);
+        if(product != null)
+        {service.deleteProduct(id);
+            return new ResponseEntity<>("Deleted", HttpStatus.OK);}
+        else return new ResponseEntity<>("Error Deleting", HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping("/api/products/search")
+    public ResponseEntity<List<Product>> searchProducts(@RequestParam String keyword)
+    {
+        List<Product> products = service.searchProducts(keyword);
+        System.out.println("Searching with " + keyword);
+        return new ResponseEntity<>(products, HttpStatus.OK);
+    }
+}
---
+++ src/main/resources/data1.sql
@@ -0,0 +1,6 @@
+INSERT INTO Product (id, name, desc, brand, price, category, date, availability, stock) VALUES
+(1, 'Rolls-Royce Phantom', 'Flagship luxury sedan with unparalleled craftsmanship.', 'Rolls-Royce', 460000.00, 'Sedan', '2025-02-01', TRUE, 5),
+(2, 'Bentley Flying Spur', 'Luxury sedan offering a blend of power and comfort.', 'Bentley', 214000.00, 'Sedan', '2025-01-15', TRUE, 7),
+(3, 'Mercedes-Benz S-Class', 'High-tech luxury sedan with cutting-edge features.', 'Mercedes-Benz', 110000.00, 'Sedan', '2025-01-20', TRUE, 10),
+(4, 'Porsche Panamera', 'Sporty luxury sedan with outstanding performance.', 'Porsche', 92000.00, 'Sedan', '2025-02-02', TRUE, 12),
+(5, 'BMW 7 Series', 'Executive sedan with advanced technology and comfort.', 'BMW', 87000.00, 'Sedan', '2025-01-30', TRUE, 15);
